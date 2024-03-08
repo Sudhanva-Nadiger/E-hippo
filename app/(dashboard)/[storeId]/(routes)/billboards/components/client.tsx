6c9929b17dff42ac41f4fc89f7e58cb5091d9f5b
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-03-08 20:04:03 +0530

    billboard table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,20 +3,22 @@
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { BillBoard } from "@/lib/schema";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
 interface BillboardClientProps {
-  data: BillBoard[];
+  data: BillboardColumn[];
 }
 
 export default function BillboardClient({
   data
-}: BillboardClientProps){
+}: BillboardClientProps) {
 
   const router = useRouter();
   const params = useParams();
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -32,7 +34,14 @@ export default function BillboardClient({
           Add New
         </Button>
       </div>
+
       <Separator />
+
+      <DataTable
+        columns={columns}
+        data={data}
+        searchKey="label"
+      />
     </>
   );
 };
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,6 +1,7 @@
 import { fetchAllBillboards } from "@/lib/actions";
 import  BillboardClient  from "./components/client";
 import Error from "@/components/Error";
+import { BillboardColumn } from "./components/columns";
 
 export default async function BillboardsPage({
   params
@@ -16,10 +17,18 @@ export default async function BillboardsPage({
 
   const billboards = response.data || [];
 
+  const formattedBillboards : BillboardColumn[] = billboards.map((billboard) => {
+    return {
+      id: billboard.id.toString(),
+      label: billboard.label,
+      createdAt: billboard.createdAt.toLocaleDateString("en-GB"),
+    };
+  })
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={billboards} />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
