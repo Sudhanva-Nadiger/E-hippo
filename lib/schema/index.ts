c892d174bdf4acfd9def6e88652e0ed17060552e
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-03-11 19:59:09 +0530

    feat: size schema

--- lib/schema/index.ts
+++ lib/schema/index.ts
@@ -1,3 +1,4 @@
 export * from './store'
 export * from './billBoards'
 export * from './category'
+export * from './size'
---
+++ lib/schema/size.ts
@@ -0,0 +1,17 @@
+import {
+    pgTable,
+    text,
+    timestamp,
+    serial,
+    integer,
+  } from "drizzle-orm/pg-core";
+import { store } from ".";
+
+export const size = pgTable("sizes", {
+    id: serial("id").primaryKey(),
+    storeId: integer("store_id").notNull().references(() => store.id),
+    name: text("name").notNull(),
+    value: text("value").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+})
