76bbf0ec6c97e674b2fc1248161a69bd156c16da
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-03-21 12:36:19 +0530

    add scripts and footer refactor

--- store/components/Footer.tsx
+++ store/components/Footer.tsx
@@ -1,31 +1,11 @@
-'use client'
-
-import { usePathname } from 'next/navigation'
-import MaxWidthWrapper from './MaxWidthWrapper'
-import { Icons } from './Icons'
 import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
 
 const Footer = () => {
-  const pathname = usePathname()
-  const pathsToMinimize = [
-    '/verify-email',
-    '/sign-up',
-    '/sign-in',
-  ]
-
   return (
     <footer className='bg-white flex-grow-0'>
       <MaxWidthWrapper>
         <div className='border-t border-gray-200'>
-          {pathsToMinimize.includes(pathname) ? null : (
-            <div className='pb-8 pt-16'>
-              <div className='flex justify-center'>
-                <Icons.logo className='h-12 w-auto' />
-              </div>
-            </div>
-          )}
-
-          {pathsToMinimize.includes(pathname) ? null : (
             <div>
               <div className='relative flex items-center px-6 py-6 sm:py-8 lg:mt-0'>
                 <div className='absolute inset-0 overflow-hidden rounded-lg'>
@@ -37,22 +17,14 @@ const Footer = () => {
 
                 <div className='text-center relative mx-auto max-w-sm'>
                   <h3 className='font-semibold text-gray-900'>
-                    Become a seller
+                    Buy now
                   </h3>
                   <p className='mt-2 text-sm text-muted-foreground'>
-                    If you&apos;d like to sell high-quality
-                    digital products, you can do so in
-                    minutes.{' '}
-                    <Link
-                      href='/sign-in?as=seller'
-                      className='whitespace-nowrap font-medium text-black hover:text-zinc-900'>
-                      Get started &rarr;
-                    </Link>
+                    Contact details
                   </p>
                 </div>
               </div>
             </div>
-          )}
         </div>
 
         <div className='py-10 md:flex md:items-center md:justify-between'>
